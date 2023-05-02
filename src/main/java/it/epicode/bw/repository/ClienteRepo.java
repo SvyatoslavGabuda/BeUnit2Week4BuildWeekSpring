c2b5764b5cf9885d8de7c453d60452f8bda28c93
AlessandroD98 <alessandro.comunicazioni@gmali.com>
2023-05-02 17:29:28 +0200

    c

--- src/main/java/it/epicode/bw/repository/ClienteRepo.java
+++ src/main/java/it/epicode/bw/repository/ClienteRepo.java
@@ -1,11 +1,16 @@
 package it.epicode.bw.repository;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import it.epicode.bw.models.Cliente;
+
 
 public interface ClienteRepo extends JpaRepository<Cliente, Long>, PagingAndSortingRepository<Cliente, Long> {
 	Optional<Cliente> findByEmail(String email);
@@ -17,4 +22,21 @@ public interface ClienteRepo extends JpaRepository<Cliente, Long>, PagingAndSort
     Boolean existsByUsername(String username);
  
     Boolean existsByEmail(String email);
+    
+    public Page<Cliente> findAll(Pageable pag);
+    
+    public List<Cliente> findByFatturatoAnnualeBetween(Double a, Double b);
+    public Page<Cliente> findByFatturatoAnnualeBetween(Pageable pag,Double a,Double b);
+    
+    public List<Cliente> findByDataInserimento(LocalDate l);
+    public Page<Cliente> findByDataInserimento(Pageable pag,LocalDate l);
+    
+    public List<Cliente> findByDataUltimoContatto(LocalDate l);
+    public Page<Cliente> findByDataUltimoContatto(Pageable pag,LocalDate l);
+    
+    public List<Cliente> findByNomeContattoContains(String s);
+    public Page<Cliente> findByNomeContattoContains(Pageable pag,String s);
+    
+    public List<Cliente> findByOrderByRagioneSocialeAsc(String s);
+    public Page<Cliente> findByOrderByRagioneSocialeAsc(Pageable pag,String s);
 }
