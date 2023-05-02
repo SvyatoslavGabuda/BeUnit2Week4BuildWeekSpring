825112651b017c084bb20315af33a97c0687e7fa
SvyatoslavGabuda <sgabuda@gmail.com>
2023-05-02 16:59:17 +0200

    controller  e service

--- src/main/java/it/epicode/bw/service/FattureService.java
+++ src/main/java/it/epicode/bw/service/FattureService.java
@@ -8,8 +8,10 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
 import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
+import it.epicode.bw.auth.exception.MyAPIException;
 import it.epicode.bw.auth.service.AuthServiceImpl;
 import it.epicode.bw.enums.StatoFattura;
 import it.epicode.bw.models.Cliente;
@@ -17,7 +19,7 @@ import it.epicode.bw.models.Fattura;
 import it.epicode.bw.repository.FattureRepo;
 
 @Service
-public  class FattureService {
+public class FattureService {
 
 	@Autowired
 	FattureRepo fattureRepo;
@@ -29,6 +31,7 @@ public  class FattureService {
 		f1.setAnno(LocalDate.now().getYear());
 		f1.setDataEmissione(LocalDate.now());
 		f1.setStadioFattura(StatoFattura.EMESSA);
+		// clinte = clienteRepo.findById(id);
 		f1.setCliente(c);
 		f1.setImporto(importo);
 		fattureRepo.save(f1);
@@ -36,83 +39,152 @@ public  class FattureService {
 
 	}
 
-	public void modificaFattura(Fattura f) {
-		fattureRepo.save(f);
+	public Fattura modificaFattura(Fattura f) {
+		if (fattureRepo.existsById(f.getId_fattura())) {
+			fattureRepo.save(f);
+			return f;
+		} else {
+			throw new MyAPIException(HttpStatus.NOT_FOUND, "Fattura non torvata");
+		}
 	}
 
-	public void eliminaFattura(Long id) {
-		fattureRepo.deleteById(id);
+	public String eliminaFattura(Long id) {
+		if (fattureRepo.existsById(id)) {
+			fattureRepo.deleteById(id);
+			return "fattura eliminata";
+
+		} else {
+			throw new MyAPIException(HttpStatus.NOT_FOUND, "Fattura non torvata");
+		}
 	}
 
 	public List<Fattura> findByCliente(Cliente c) {
+		// metodo che ricerca se il cliente esiste
 		List<Fattura> f = fattureRepo.findByCliente(c);
 		return f;
 
 	}
 
-	public Page<Fattura> findByClientePagable(Pageable pag,Cliente c) {
-		Page<Fattura> f = fattureRepo.findByCliente((org.springframework.data.domain.Pageable) pag,c);
+	public Page<Fattura> findByClientePagable(Pageable pag, Cliente c) {
+		Page<Fattura> f = fattureRepo.findByCliente((org.springframework.data.domain.Pageable) pag, c);
 		return f;
 
 	}
-	
+
 	public Fattura findById(long id) {
-		return fattureRepo.findById(id).get();
-		
-		
+		if (fattureRepo.existsById(id)) {
+			return fattureRepo.findById(id).get();
+		} else {
+			throw new MyAPIException(HttpStatus.NOT_FOUND, "Fattura non torvata");
+		}
+
 	}
-	public List<Fattura> findAllFatture(){
-		return  fattureRepo.findAll();
-		
-		
+
+	public List<Fattura> findAllFatture() {
+		if (!fattureRepo.findAll().isEmpty()) {
+			return fattureRepo.findAll();
+
+		} else {
+			throw new MyAPIException(HttpStatus.NOT_FOUND, "Fatture non torvate");
+		}
+
 	}
-	
-	
-	public List<Fattura> findByStadioFattura(StatoFattura stadioFattura){
+
+	public List<Fattura> findByStadioFattura(StatoFattura stadioFattura) {
+		if (!(stadioFattura instanceof StatoFattura)) {
+			throw new MyAPIException(HttpStatus.NOT_FOUND, "valore inserito non valido");
+		}
+		if (fattureRepo.findByStadioFattura(stadioFattura).isEmpty()) {
+			throw new MyAPIException(HttpStatus.NOT_FOUND, "Fatture non torvate");
+		}
 		List<Fattura> f = fattureRepo.findByStadioFattura(stadioFattura);
 		return f;
-		
+
 	}
-	public Page<Fattura> findByStadioFatturaPageable(Pageable pag, StatoFattura stadioFattura){
-		Page<Fattura> f = fattureRepo.findByStadioFattura((org.springframework.data.domain.Pageable) pag, stadioFattura);
+
+	public Page<Fattura> findByStadioFatturaPageable(Pageable pag, StatoFattura stadioFattura) {
+		if (!(stadioFattura instanceof StatoFattura)) {
+			throw new MyAPIException(HttpStatus.BAD_REQUEST, "valore inserito non valido");
+		}
+		if (fattureRepo.findByStadioFattura(stadioFattura).isEmpty()) {
+			throw new MyAPIException(HttpStatus.NOT_FOUND, "Fatture non torvate");
+		}
+		Page<Fattura> f = fattureRepo.findByStadioFattura((org.springframework.data.domain.Pageable) pag,
+				stadioFattura);
 		return f;
-		
+
 	}
-	
-	public List<Fattura> findByDataEmissione(LocalDate data){
-		List<Fattura> f= fattureRepo.findByDataEmissione(data);
+
+	public List<Fattura> findByDataEmissione(LocalDate data) {
+		if (!(data instanceof LocalDate)) {
+			throw new MyAPIException(HttpStatus.BAD_REQUEST, "valore inserito non valido");
+		}
+		if (fattureRepo.findByDataEmissione(data).isEmpty()) {
+			throw new MyAPIException(HttpStatus.NOT_FOUND, "Fatture non torvate");
+		}
+		List<Fattura> f = fattureRepo.findByDataEmissione(data);
 		return f;
-		
+
 	}
-	
-	public Page<Fattura> findByDataEmissionePageable (Pageable pag,LocalDate data){
-		Page<Fattura> f= fattureRepo.findByDataEmissione((org.springframework.data.domain.Pageable) pag,data);
+
+	public Page<Fattura> findByDataEmissionePageable(Pageable pag, LocalDate data) {
+		if (!(data instanceof LocalDate)) {
+			throw new MyAPIException(HttpStatus.BAD_REQUEST, "valore inserito non valido");
+		}
+		if (fattureRepo.findByDataEmissione(data).isEmpty()) {
+			throw new MyAPIException(HttpStatus.NOT_FOUND, "Fatture non torvate");
+		}
+		Page<Fattura> f = fattureRepo.findByDataEmissione((org.springframework.data.domain.Pageable) pag, data);
 		return f;
-		
+
 	}
-	public List<Fattura> findByDataAnno(Integer a){
-		List<Fattura> f= fattureRepo.findByAnno(a);
+
+	public List<Fattura> findByDataAnno(Integer a) {
+		if (!(a instanceof Integer)) {
+			throw new MyAPIException(HttpStatus.BAD_REQUEST, "valore inserito non valido");
+		}
+		if (fattureRepo.findByAnno(a).isEmpty()) {
+			throw new MyAPIException(HttpStatus.NOT_FOUND, "Fatture non torvate");
+		}
+		List<Fattura> f = fattureRepo.findByAnno(a);
 		return f;
-		
+
 	}
-	
-	public Page<Fattura> findByDataAnnoPageable (Pageable pag,Integer a){
-		Page<Fattura> f= fattureRepo.findByAnno((org.springframework.data.domain.Pageable) pag,a);
+
+	public Page<Fattura> findByDataAnnoPageable(Pageable pag, Integer a) {
+		if (!(a instanceof Integer)) {
+			throw new MyAPIException(HttpStatus.BAD_REQUEST, "valore inserito non valido");
+		}
+		if (fattureRepo.findByAnno(a).isEmpty()) {
+			throw new MyAPIException(HttpStatus.NOT_FOUND, "Fatture non torvate");
+		}
+		Page<Fattura> f = fattureRepo.findByAnno((org.springframework.data.domain.Pageable) pag, a);
 		return f;
-		
+
 	}
-	public List<Fattura> findByImporto(Double a, Double b){
-		List<Fattura> f= fattureRepo.findByImportoBetween(a,b);
+
+	public List<Fattura> findByImporto(Double a, Double b) {
+		if (!(a instanceof Double && b instanceof Double)) {
+			throw new MyAPIException(HttpStatus.BAD_REQUEST, "valore inserito non valido");
+		}
+		if (fattureRepo.findByImportoBetween(a, b).isEmpty()) {
+			throw new MyAPIException(HttpStatus.NOT_FOUND, "Fatture non torvate");
+		}
+		List<Fattura> f = fattureRepo.findByImportoBetween(a, b);
 		return f;
-		
+
 	}
-	
-	public Page<Fattura> findByDataImportoPageable (Pageable pag,Double a, Double b){
-		Page<Fattura> f= fattureRepo.findByImportoBetween((org.springframework.data.domain.Pageable) pag,a,b);
+
+	public Page<Fattura> findByImportoPageable(Pageable pag, Double a, Double b) {
+		if (!(a instanceof Double && b instanceof Double)) {
+			throw new MyAPIException(HttpStatus.BAD_REQUEST, "valore inserito non valido");
+		}
+		if (fattureRepo.findByImportoBetween(a, b).isEmpty()) {
+			throw new MyAPIException(HttpStatus.NOT_FOUND, "Fatture non torvate");
+		}
+		Page<Fattura> f = fattureRepo.findByImportoBetween((org.springframework.data.domain.Pageable) pag, a, b);
 		return f;
-		
+
 	}
-	
-	
-	
+
 }
