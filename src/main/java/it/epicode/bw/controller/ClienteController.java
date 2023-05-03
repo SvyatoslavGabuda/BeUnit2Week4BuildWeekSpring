ac71e600feab65517f94140a45bd22b3ef18ab04
bebob94 <bebo.macis@gmail.com>
2023-05-03 15:21:45 +0200

    add

---
+++ src/main/java/it/epicode/bw/controller/ClienteController.java
@@ -0,0 +1,88 @@
+package it.epicode.bw.controller;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import it.epicode.bw.models.Cliente;
+import it.epicode.bw.models.Indirizzo;
+import it.epicode.bw.service.ClientiService;
+
+@RestController
+@RequestMapping("/Cliente")
+@CrossOrigin(origins = "*", maxAge = 6000000)
+public class ClienteController {
+	
+	@Autowired ClientiService clientiService;
+
+	@GetMapping("/{id}")
+	@PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
+	public ResponseEntity<Cliente> trovaClienteTramiteId(@PathVariable Long id) {
+		return new ResponseEntity<Cliente>(clientiService.findById(id), HttpStatus.OK);
+	}
+	
+	@GetMapping
+	@PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
+	public ResponseEntity<List<Cliente>> trovaClientiAll() {
+		return new ResponseEntity<List<Cliente>>(clientiService.findAllClienti(), HttpStatus.OK);
+	}
+	
+	@GetMapping("/dataInserimento/{anno}/{mese}/{giorno}") 
+	@PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
+	public ResponseEntity<List<Cliente>> trovaClienteByDataInseriemento(@PathVariable int anno, @PathVariable int mese,
+			@PathVariable int giorno) {
+		return new ResponseEntity<List<Cliente>>(clientiService.findByDateIns(LocalDate.of(anno, mese, giorno)),
+				HttpStatus.OK);
+	}
+	
+	@GetMapping("/fatturato/{imp}/{imp2}") 
+	@PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
+	public ResponseEntity<List<Cliente>> trovaFatturato(@PathVariable Double imp,@PathVariable Double imp2) {
+		return new ResponseEntity<List<Cliente>>(clientiService.findByFatturato(imp, imp2), HttpStatus.OK);
+	}
+	
+	@GetMapping("/dataUltimoCont/{anno}/{mese}/{giorno}") 
+	@PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
+	public ResponseEntity<List<Cliente>> trovaClienteperUltimoCont(@PathVariable int anno, @PathVariable int mese,
+			@PathVariable int giorno) {
+		return new ResponseEntity<List<Cliente>>(clientiService.findByDateLastCont(LocalDate.of(anno, mese, giorno)),
+				HttpStatus.OK);
+	}
+	
+	@GetMapping("/name/{imp}") 
+	@PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
+	public ResponseEntity<List<Cliente>> trovaNome(@PathVariable String imp) {
+		return new ResponseEntity<List<Cliente>>(clientiService.findByNameContains(imp), HttpStatus.OK);
+	}
+	
+	@GetMapping("/sedeLegale/{imp}") 
+	@PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
+	public ResponseEntity<List<Cliente>> trovaSedeLegale(@PathVariable Indirizzo imp) {
+		return new ResponseEntity<List<Cliente>>(clientiService.findBySedeLegale(imp), HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/{id}")
+	@PreAuthorize("hasRole('ADMIN')")
+	public ResponseEntity<String> eliminaCliente( @PathVariable Long id){
+		return new ResponseEntity<String>(clientiService.eliminaCliente(id), HttpStatus.OK);
+	}
+	
+	@PutMapping()
+	@PreAuthorize("hasRole('ADMIN')")
+	public ResponseEntity<?> updateCliente(@RequestBody Cliente c){
+		return new ResponseEntity<Cliente>(clientiService.modificaCliente(c),HttpStatus.OK);
+	}
+	
+}
--- src/main/java/it/epicode/bw/repository/ClienteRepo.java
+++ src/main/java/it/epicode/bw/repository/ClienteRepo.java
@@ -10,6 +10,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import it.epicode.bw.models.Cliente;
+import it.epicode.bw.models.Indirizzo;
 
 
 public interface ClienteRepo extends JpaRepository<Cliente, Long>, PagingAndSortingRepository<Cliente, Long> {
@@ -26,17 +27,32 @@ public interface ClienteRepo extends JpaRepository<Cliente, Long>, PagingAndSort
     public Page<Cliente> findAll(Pageable pag);
     
     public List<Cliente> findByFatturatoAnnualeBetween(Double a, Double b);
+    public List<Cliente> findByFatturatoAnnualeOrderByFatturatoAnnualeAsc(Double a);
     public Page<Cliente> findByFatturatoAnnualeBetween(Pageable pag,Double a,Double b);
+    public Page<Cliente> findByFatturatoAnnualeOrderByFatturatoAnnualeAsc(Pageable pag,Double a);
     
     public List<Cliente> findByDataInserimento(LocalDate l);
+    public List<Cliente> findByDataInserimentoOrderByDataInserimentoAsc(LocalDate l);
     public Page<Cliente> findByDataInserimento(Pageable pag,LocalDate l);
+    public Page<Cliente> findByDataInserimentoOrderByDataInserimentoAsc(Pageable pag,LocalDate l);
     
     public List<Cliente> findByUltimoContatto(LocalDate l);
+    public List<Cliente> findByUltimoContattoOrderByUltimoContattoAsc(LocalDate l);
     public Page<Cliente> findByUltimoContatto(Pageable pag,LocalDate l);
+    public Page<Cliente> findByUltimoContattoOrderByUltimoContattoAsc(Pageable pag,LocalDate l);
     
     public List<Cliente> findByNomeContattoContains(String s);
+    public List<Cliente> findByNomeContattoOrderByNomeContattoAsc(String s);
     public Page<Cliente> findByNomeContattoContains(Pageable pag,String s);
+    public Page<Cliente> findByNomeContattoOrderByNomeContattoAsc(Pageable pag,String s);
     
     public List<Cliente> findByRagioneSocialeOrderByRagioneSocialeAsc(String s);
     public Page<Cliente> findByRagioneSocialeOrderByRagioneSocialeAsc(Pageable pag,String s);
+    
+    
+    public List<Cliente> findBySedeLegale(Indirizzo sedeLegale);
+    public List<Cliente> findBySedeLegaleOrderBySedeLegaleAsc(Indirizzo sedeLegale);
+    public Page<Cliente> findBySedeLegale(Pageable pag, Indirizzo sedeLegale);
+    public Page<Cliente> findBySedeLegaleOrderBySedeLegaleAsc(Pageable pag,Indirizzo sedeLegale);
+    
 }
